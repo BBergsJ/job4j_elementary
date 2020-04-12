39d5f78daf09c5bf996453d12793be89d57b6993
BBergsJ <goldlike888@gmail.com>
2020-04-12 23:34:55 +0300

    1. Модульные тесты.[#272583]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -18,7 +18,7 @@ public class Converter {
         System.out.println("140 rubles are 2. Test result : " + passed);
 
         in = 240;
-        expected = 5;
+        expected = 4;
         out = Converter.rubleToDollar(in);
         passed = expected == out;
         System.out.println("240 rubles are 4. Test result : " + passed);
