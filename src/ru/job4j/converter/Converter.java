3dcead5e20890905af286b09d7f44a0db1e840d9
BBergsJ <goldlike888@gmail.com>
2020-04-15 21:57:09 +0300

    5.8. JavaDoc. Code convention[#272607]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,15 +1,33 @@
 package ru.job4j.converter;
 
+/**
+ * Converter.
+ *
+ * @author Dmitry Emelyanov (goldlike888@gmail.com)
+ */
 public class Converter {
-
+    /**
+     *Method rubleToEuro.
+     * @param value Ruble to Euro.
+     * @return Euros.
+     */
     public static int rubleToEuro(int value) {
         return value / 70;
     }
 
+    /**
+     * Method rubleToDollar.
+     * @param value Ruble to Dollar.
+     * @return Dollars.
+     */
     public static int rubleToDollar(int value) {
         return value / 60;
     }
 
+    /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         int in = 140;
         int expected = 2;
