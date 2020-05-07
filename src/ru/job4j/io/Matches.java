18c327a83f4ed5117aa79bc8f12b83cc2b83b3d6
BBergsJ <goldlike888@gmail.com>
2020-05-08 00:12:13 +0300

    2. Scanner и чтение числа из консоли. [#272646]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,23 +6,28 @@ public class Matches {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int matches = 11;
+        boolean turnOne = true;
         System.out.println("Игра \"Спички\"");
         while (matches > 0) {
-            System.out.println("Игрок 1, заберите от 1 до 3 спичек: ");
-            int playerOne = Integer.valueOf(scanner.nextLine());
-            matches -= playerOne;
-            if (matches <= 0) {
-                System.out.println("Победил Игрок 2.");
-            }
-            System.out.println("Осталось " + matches + " спичек.");
-            System.out.println("Игрок 2, заберите от 1 до 3 спичек: ");
-            int playerTwo = Integer.valueOf(scanner.nextLine());
-            matches -= playerTwo;
-            if (matches <= 0) {
-                System.out.println("Победил Игрок 1.");
+            if (turnOne) {
+                System.out.println("Игрок 1, заберите от 1 до 3 спичек: ");
+                int playerOne = Integer.valueOf(scanner.nextLine());
+                matches -= playerOne;
+                turnOne = false;
+            } else {
+                System.out.println("Игрок 2, заберите от 1 до 3 спичек: ");
+                int playerTwo = Integer.valueOf(scanner.nextLine());
+                matches -= playerTwo;
+                turnOne = true;
             }
             System.out.println("Осталось " + matches + " спичек.");
         }
-        System.out.println("Спички кончились, конец игры.");
+        System.out.println("Спички кончились.");
+        if (turnOne) {
+            System.out.println("Победил Игрок 1!" + System.lineSeparator() + "Конец игры.");
+        } else {
+            System.out.println("Победил Игрок 2!" + System.lineSeparator() + "Конец игры.");
+        }
+
     }
 }
