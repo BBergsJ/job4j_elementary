a60307c4882fc0232f4a3454cfc2e9847f48d3e6
BBergsJ <goldlike888@gmail.com>
2020-05-25 23:58:25 +0300

    Кидаем исключение - throw new RuntimeException

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -7,7 +7,7 @@ public class Fact {
 
     public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("'N' should be more than 0");
+            throw new IllegalArgumentException("N could not be less then 0");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
---
+++ test/ru/job4j/ex/FactTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test
+    public void calc() {
+    }
+}
