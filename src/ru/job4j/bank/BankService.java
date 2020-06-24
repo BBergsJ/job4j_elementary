ba8a5813e357944a5f13038b4bba895b7139e6fb
BBergsJ <goldlike888@gmail.com>
2020-06-24 20:51:24 +0300

    Банковские переводы

---
+++ src/ru/job4j/bank/BankService.java
@@ -0,0 +1,56 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (!users.get(user).equals(account)) {
+            users.get(user).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        if (findByPassport(passport) != null) {
+            for (Account account : users.get(findByPassport(passport))) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+
+        Account destination = findByRequisite(destPassport, destRequisite);
+        Account source = findByRequisite(srcPassport, srcRequisite);
+
+        if (destination.equals(null) || source.getBalance() < amount) {
+            return false;
+        } else  {
+            destination.setBalance(destination.getBalance() + amount);
+            source.setBalance(source.getBalance() - amount);
+            return true;
+        }
+    }
+}
