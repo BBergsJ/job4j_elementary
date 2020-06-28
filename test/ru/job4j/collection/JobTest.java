789014f5f296c73cce50d5a8de6c02bdb1a5cf5b
BBergsJ <goldlike888@gmail.com>
2020-06-28 20:45:02 +0300

    Комбинированный компаратор

---
+++ test/ru/job4j/collection/JobTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class JobTest {
+    @Test
+    public void whenCompatorByNameAndProrityDesc() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndProrityInc() {
+        Comparator<Job> cmpNamePriority = new JobIncByName().thenComparing(new JobIncByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameInc() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        List<Job> sorted = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        Collections.sort(jobs, new JobIncByName());
+        assertThat(jobs, is(sorted));
+    }
+
+    @Test
+    public void whenCompatorByPriorityInc() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        List<Job> sorted = Arrays.asList(
+                new Job("X task", 0),
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 2),
+                new Job("Fix bug", 4)
+        );
+        Collections.sort(jobs, new JobIncByPriority());
+        assertThat(jobs, is(sorted));
+    }
+}
