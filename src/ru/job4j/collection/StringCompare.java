2fccaefec2bde3b6d713cf31d4ed3d90acb966b1
BBergsJ <goldlike888@gmail.com>
2020-06-30 00:58:36 +0300

    Компаратор для строк

--- src/ru/job4j/collection/StringCompare.java
+++ src/ru/job4j/collection/StringCompare.java
@@ -7,18 +7,15 @@ public class StringCompare implements Comparator<String> {
     public int compare(String left, String right) {
         int minLenght = Math.min(left.length(), right.length());
         int result = 0;
-//        int counter = 0;
-//            while (counter < minLenght) {
-//                if (left.charAt(counter) != right.charAt(counter)) {
-//                    return -1;
-//                }
-//                counter++;
-//            }
-        for (int i = 0; i < minLenght; i++) {
-            result = Character.compare(left.charAt(i), right.charAt(i));
-            if (result != 0) {
-                return Integer.compare(left.length(), right.length());
+
+            for (int i = 0; i < minLenght; i++) {
+                result = Character.compare(left.charAt(i), right.charAt(i));
+                if (result != 0) {
+                    return result;
+                }
             }
+        if (left.length() != right.length()) {
+            result = Integer.compare(left.length(), right.length());
         }
         return result;
     }
