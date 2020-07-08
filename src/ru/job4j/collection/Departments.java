6b22f351c1194a09f48c1dc7aa7a6bf8c31e6600
BBergsJ <goldlike888@gmail.com>
2020-07-08 23:12:28 +0300

    Отсортировать департаменты

---
+++ src/ru/job4j/collection/Departments.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                tmp.add(start + el);
+                start = el + "/";
+//                tmp.add(start + "/" + el);
+//                start = el;
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+}
