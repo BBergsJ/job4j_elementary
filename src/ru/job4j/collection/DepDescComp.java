65313d5ad053dc20301c0aa71af21cc4bafd4544
BBergsJ <goldlike888@gmail.com>
2020-07-09 21:46:33 +0300

    Компаратор для строк

--- src/ru/job4j/collection/DepDescComp.java
+++ src/ru/job4j/collection/DepDescComp.java
@@ -5,9 +5,23 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-//        if (o1.split("/") == o2.split("/")) {
-//
-//        }
-        return 0;
+
+        String[] oOne = o1.split("/");
+        String[] oTwo = o2.split("/");
+        int minLenght = Math.min(oOne.length, oTwo.length);
+        int result = 0;
+
+        for (int i = 0; i < minLenght; i++) {
+            result = oOne[i].compareTo(oTwo[i]);
+            if (result != 0) {
+                break;
+            }
+        }
+
+        if (oOne.length != oTwo.length) {
+            result = Integer.compare(oOne.length, oTwo.length);
+        }
+
+        return result;
     }
 }
