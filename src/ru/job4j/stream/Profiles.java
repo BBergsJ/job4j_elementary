8773110062d010e9b052251db1359995910f0aa2
BBergsJ <goldlike888@gmail.com>
2020-07-25 20:57:41 +0300

    Уникальность элементов и сортировка

--- src/ru/job4j/stream/Profiles.java
+++ src/ru/job4j/stream/Profiles.java
@@ -1,12 +1,16 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         List<Address> result = profiles.stream()
-                .map(Profile::getAddress).collect(Collectors.toList());
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
+                .collect(Collectors.toList());
         return result;
     }
 }
