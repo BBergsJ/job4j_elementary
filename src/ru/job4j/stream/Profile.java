b2b4b35ace63e0a1d6b4f116ba3d9a315630cbb3
BBergsJ <goldlike888@gmail.com>
2020-07-25 20:29:06 +0300

    Список адресов

--- src/ru/job4j/stream/Profile.java
+++ src/ru/job4j/stream/Profile.java
@@ -1,9 +1,5 @@
 package ru.job4j.stream;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Profile {
     private Address address;
     private String name;
@@ -15,11 +11,5 @@ public class Profile {
 
     public Address getAddress() {
         return address;
-    }
-
-    public static List<Address> collect(List<Profile> profiles) {
-        List<Address> result = profiles.stream()
-                .map(Profile::getAddress).collect(Collectors.toList());
-        return result;
     }
 }
