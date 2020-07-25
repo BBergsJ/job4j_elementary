aa018269681f7c318c2256af2875936938e18d5d
BBergsJ <goldlike888@gmail.com>
2020-07-25 21:01:32 +0300

    Уникальность элементов и сортировка

--- src/ru/job4j/stream/Address.java
+++ src/ru/job4j/stream/Address.java
@@ -9,13 +9,6 @@ public class Address {
     private int home;
     private int apartment;
 
-    public Address(String city, String street, int home, int apartment) {
-        this.city = city;
-        this.street = street;
-        this.home = home;
-        this.apartment = apartment;
-    }
-
     public String getCity() {
         return city;
     }
