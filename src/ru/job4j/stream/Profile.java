ef3b4cffb2bff6ac56139d730433dc9ee4ab9d91
BBergsJ <goldlike888@gmail.com>
2020-07-25 21:00:45 +0300

    Уникальность элементов и сортировка

--- src/ru/job4j/stream/Profile.java
+++ src/ru/job4j/stream/Profile.java
@@ -2,12 +2,6 @@ package ru.job4j.stream;
 
 public class Profile {
     private Address address;
-    private String name;
-
-    public Profile(String name, Address address) {
-        this.name = name;
-        this.address = address;
-    }
 
     public Address getAddress() {
         return address;
