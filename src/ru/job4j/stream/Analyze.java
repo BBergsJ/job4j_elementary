17f44f640dacef6548ccab23a0f5481bf4c7755f
BBergsJ <goldlike888@gmail.com>
2020-08-04 23:02:29 +0300

    Крестики-нолики на JavaFX

---
+++ src/ru/job4j/stream/Analyze.java
@@ -0,0 +1,33 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.getSubjects().stream())
+                .mapToDouble(Subject::getScore)
+                .average()
+                .getAsDouble();
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.getSubjects().stream()).;
+//        return List.of();
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return null;
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return null;
+    }
+}
