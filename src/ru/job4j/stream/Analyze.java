316bb488b0d84229d41997b811a93177392787dd
BBergsJ <goldlike888@gmail.com>
2020-08-14 00:30:48 +0300

    Stream API улучшения

--- src/ru/job4j/stream/Analyze.java
+++ src/ru/job4j/stream/Analyze.java
@@ -1,7 +1,9 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,25 +11,33 @@ import java.util.stream.Stream;
 public class Analyze {
 
     public static double averageScore(Stream<Pupil> stream) {
-        return stream.flatMap(x -> x.getSubjects().stream())
+        return stream
+                .flatMap(x -> x.getSubjects().stream())
                 .mapToDouble(Subject::getScore)
                 .average()
                 .getAsDouble();
     }
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
-        return stream.map(x -> new Tuple(
+        return stream
+                .map(x -> new Tuple(
                         x.getName(),
-                        x.getSubjects().stream().mapToDouble(Subject::getScore).average().getAsDouble()
-                )).collect(Collectors.toList());
+                        x.getSubjects().stream().mapToDouble(Subject::getScore).average().getAsDouble()))
+                .collect(Collectors.toList());
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
-//        return stream.flatMap(x -> new Tuple(
-//                x.getSubjects().,
-//                x.getSubjects().stream().mapToDouble(Subject::getScore)
-//        )).collect(Collectors.toList());
-        return stream.collect(Collectors.toMap(Subject::getName, ));
+        return stream
+                .flatMap(x -> x.getSubjects().stream())
+                .collect(Collectors.toMap(
+                        Subject::getName,
+                        Subject::getScore,
+                        (a, b) -> (a + b) / 2))
+                .entrySet()
+                .stream()
+                .map(x -> new Tuple(x.getKey(), x.getValue()))
+                .sorted(Comparator.comparing(Tuple::getName).reversed())
+                .collect(Collectors.toList());
     }
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
